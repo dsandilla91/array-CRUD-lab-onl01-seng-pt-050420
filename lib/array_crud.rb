c8eedaf4cefe9ff37ff17c54a7cc760221a6e118
Deborah Santillan <s.debb@yahoo.com>
2020-05-11 20:04:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,76 @@
-def create_an_empty_array
-  
-end
+def create_an_empty_array 
+  []
+end 
 
 def create_an_array
-  
-end
+array(one, two, three, four) 
+end 
 
 def add_element_to_end_of_array(array, element)
-  
-end
+array.push(element)  
+end 
 
 def add_element_to_start_of_array(array, element)
-  
+array.unshift(element) 
 end
 
-def remove_element_from_end_of_array(array)
-  
-end
+def remove_element_from_end_of_array(array) 
+array.pop(item)  
+end 
 
 def remove_element_from_start_of_array(array)
-  
-end
+array.shift(item)   
+end 
+ 
+def retrieve_element_from_index(array, index number)
+array(2) 
+end 
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+def retrieve_first_element_from_array(array) 
+array.first 
+end 
 
-def retrieve_first_element_from_array(array)
-  
-end
+def retrieve_last_element_from_array(array) 
+array.last 
+end 
 
-def retrieve_last_element_from_array(array)
-  
-end
+# def kind_of_animals
+# ["dog", "cat", "sheep", "bird"]  
+# end
+
+# def traffic_lights["red", "green"]
+# end
+# traffic_lights << "yellow"
+# puts traffic_lights.inspect 
+# end
+
+# def store_furniture["vanity", "chair", "drawer"]
+#   store_furniture.unshift("table")
+#   puts store_furniture.inspect
+# end
+
+# def zoo_creatures["giraffe", "elephant", "lion", "whale"]
+#   lion_creature = zoo_creatures.pop
+#   puts zoo_creatures.inspect 
+#   puts lion_creature
+# end
+
+# def fast_food["McDonalds", "Wendys", "Burger King"]
+#   "McDonalds" = fast_food.shift
+#   puts fast_food.inspect
+#   puts "McDonalds"
+# end
+
+# def department_store["macys", "jcpenny", "nordstrom"] 
+#   department_store[0]
+#   department_store[1]
+#   department_store[2]
+# end 
+
+# def bear_type["polar", "black", "brown"]
+# bear_type.first
+# end
+
+# def flan_ingredients["milk", "egg", "sugar"]
+# flan_ingredients.last 
+# end
